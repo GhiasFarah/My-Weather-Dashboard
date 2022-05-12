87e9e539cbe5f9aaa4334466a46c3a8d824ba4ad
farah <farah9hias@gmail.com>
2022-05-11 18:14:53 -0700

    added moment.js

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ var url1 = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key
 var search = $('.search')
 search.click(() => getW())
   function getW() {
+
     fetch(url1)
       .then(res => res.json())
       .then(d => {
         console.log(d)
       })
   }
+  $('#date0').text(`${moment().format('L')}`)
+  $('#date1').text(`${moment().add(1, 'days').format('L')}`)
+  $('#date2').text(`${moment().add(2,'days').format('L')}`)
+  $('#date3').text(`${moment().add(3,'days').format('L')}`)
+  $('#date4').text(`${moment().add(4,'days').format('L')}`)
+  $('#date5').text(`${moment().add(5,'days').format('L')}`)
